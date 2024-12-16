ad9da9712adb0b7d0f126e72ffbb923fcaf43196
Sardar <sardarkhurram121@gmail.com>
2024-12-16 22:51:26 +0500

    Commit1: created portfolio app ready to push

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   darkMode: 'class',
   content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
-// module.exports = {
-//   theme: {
-//     extend: {
-//       animation: {
-//         'spin-slow': 'spin 8s linear infinite',
-//         'slide-left': 'slideLeft 3s ease-in-out infinite',
-//       },
-//       keyframes: {
-//         slideLeft: {
-//           '0%, 100%': { transform: 'translateX(0)' },
-//           '50%': { transform: 'translateX(15px)' },
-//         },
-//       },
-//     },
-//   },
-// };
+};
